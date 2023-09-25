3937b242b54282dc50ba1a654807f5c1c77adc8c
Jin959(J Lee) <84704897+Jin959@users.noreply.github.com>
2023-09-25 23:25:09 +0900

    테스트 케이스 세분화
    - 추가 요구사항 발생시 테스트 케이스 작성
    - 경계값 테스트
    - 예외 테스트

--- src/main/java/sample/cafekiosk/unit/CafeKiosk.java
+++ src/main/java/sample/cafekiosk/unit/CafeKiosk.java
@@ -17,6 +17,19 @@ public class CafeKiosk {
         beverages.add(beverage);
     }
 
+    /**
+     * 한 종류의 음료를 여러 잔 담는 요구사항 발생
+     * count == -1 일 같은 경우 예외 처리를 하기로 했다고 하자.
+     */
+    public void add(Beverage beverage, int count) {
+        if (count <= 0) {
+            throw new IllegalArgumentException("음료는 한잔 이상 주문 하셔야 합니다.");
+        }
+        for (int i = 0; i < count; i++) {
+            beverages.add(beverage);
+        }
+    }
+
     public void remove(Beverage beverage) {
         beverages.remove(beverage);
     }
--- src/test/java/sample/cafekiosk/unit/CafeKioskTest.java
+++ src/test/java/sample/cafekiosk/unit/CafeKioskTest.java
@@ -4,7 +4,7 @@ import org.junit.jupiter.api.Test;
 import sample.cafekiosk.unit.beverages.Americano;
 import sample.cafekiosk.unit.beverages.Latte;
 
-import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.*;
 
 class CafeKioskTest {
 
@@ -24,6 +24,37 @@ class CafeKioskTest {
 
         assertThat(cafeKiosk.getBeverages()).hasSize(1);
         assertThat(cafeKiosk.getBeverages().get(0).getName()).isEqualTo("아메리카노");
+    }
+
+    /**
+     * 한 종류의 음료를 여러 잔 담는 요구사항에 대한 경계값 테스트
+     * 2개일 때가 경계
+     * 일단 대충 여러 잔을 담을때, 같은 인스턴스를 사용한다고 하자.
+     */
+    @Test
+    void addSeveralBeverages() {
+        CafeKiosk cafeKiosk = new CafeKiosk();
+        Americano americano = new Americano();
+
+        cafeKiosk.add(americano, 2);
+
+        // 같은 인스턴스가 두번 추가 되었는가?
+        assertThat(cafeKiosk.getBeverages().get(0)).isEqualTo(americano);
+        assertThat(cafeKiosk.getBeverages().get(0)).isEqualTo(americano);
+    }
+
+    /**
+     * 한 종류의 음료를 여러 잔 담는 요구사항에 대한 예외 테스트
+     * CafeKiosk 클래스에서 예외를 던지는 것으로 구현하기로 했었다.
+     */
+    @Test
+    void addZeroBeverages() {
+        CafeKiosk cafeKiosk = new CafeKiosk();
+        Americano americano = new Americano();
+
+        assertThatThrownBy(() -> cafeKiosk.add(americano, 0))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage("음료는 한잔 이상 주문 하셔야 합니다.");   // 한술 더뜨면 메세지 내용까지 검증 할 수 있다.
     }
 
     @Test

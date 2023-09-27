4d414c69cb147477bdb9050dab623826ef4d45f4
Jin959(J Lee) <84704897+Jin959@users.noreply.github.com>
2023-09-27 17:43:45 +0900

    TDD 사이클 진입  전 기본 구현
    - BUSINESS 로직 RED, GREEN, REFACTORING 사이클 진입 전 컨트롤러까지 기본 구현

---
+++ src/main/java/sample/cafekiosk/spring/domain/order/Order.java
@@ -0,0 +1,34 @@
+package sample.cafekiosk.spring.domain.order;
+
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import sample.cafekiosk.spring.domain.BaseEntity;
+import sample.cafekiosk.spring.domain.orderproduct.OrderProduct;
+
+import javax.persistence.*;
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
+
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Entity
+@Table(name = "orders") // order 가 SQL 예약어라서 테이블 명 변경
+public class Order extends BaseEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long Id;
+
+    @Enumerated(EnumType.STRING)
+    private OrderStatus orderStatus;
+
+    private int totalPrice;
+
+    private LocalDateTime registeredDateTime;
+
+    @OneToMany(mappedBy = "order", cascade = CascadeType.ALL)
+    private List<OrderProduct> orderProducts = new ArrayList<>();
+
+}

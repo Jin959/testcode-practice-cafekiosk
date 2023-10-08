9457f8c098ae08d2c7ca97e1e07cb13d8bff5efc
Jin959(J Lee) <84704897+Jin959@users.noreply.github.com>
2023-10-09 01:39:44 +0900

    주문생성 테스트 - GREEN
    - RED to GREEN, GREEN 진입
    - 서비스 로직을 작성하면서 다시 작은 단위의 테스트 TDD 수행
    	- findByProductNumberIn Repository TDD 수행
    	- Order 유닛 테스트 수행
    - 테스트 하기 어려운 영역인 주문등록 시간 registeredDateTime 분리

--- src/test/java/sample/cafekiosk/spring/api/service/order/OrderServiceTest.java
+++ src/test/java/sample/cafekiosk/spring/api/service/order/OrderServiceTest.java
@@ -3,8 +3,8 @@ package sample.cafekiosk.spring.api.service.order;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 import sample.cafekiosk.spring.api.service.order.request.OrderCreateRequest;
 import sample.cafekiosk.spring.api.service.order.response.OrderResponse;
 import sample.cafekiosk.spring.domain.product.Product;
@@ -16,10 +16,10 @@ import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.tuple;
-import static org.junit.jupiter.api.Assertions.*;
 import static sample.cafekiosk.spring.domain.product.ProductSellingStatus.*;
 import static sample.cafekiosk.spring.domain.product.ProductType.HANDMADE;
 
+@ActiveProfiles("test")
 @SpringBootTest
 //@DataJpaTest  // orderService 빈을 조회하지 못해서 SpringBootTest를 사용해야함.
 class OrderServiceTest {
@@ -34,23 +34,25 @@ class OrderServiceTest {
     @DisplayName("주문번호 리스트를 받아 주문을 생성한다.")
     void createOrder() {
         // given
+        LocalDateTime registeredDateTime = LocalDateTime.now();
+
         Product product1 = createProduct(HANDMADE, "001", 1000);
         Product product2 = createProduct(HANDMADE, "002", 3000);
         Product product3 = createProduct(HANDMADE, "003", 5000);
         productRepository.saveAll(List.of(product1, product2, product3));
 
         OrderCreateRequest request = OrderCreateRequest.builder()
-                .productNumbers(List.of("001", "002", "003"))
+                .productNumbers(List.of("001", "002"))
                 .build();
 
         // when
-        OrderResponse orderResponse = orderService.createOrder(request);
+        OrderResponse orderResponse = orderService.createOrder(request, registeredDateTime);
 
         // then
         assertThat(orderResponse.getId()).isNotNull();  // 얼마인지는 안중요하고 id 값이 들어있는지만 확인하면 된다.
         assertThat(orderResponse)
                 .extracting("registeredDateTime", "totalPrice")
-                .contains(LocalDateTime.now(), 4000);   // 주문등록 시간을 뭐로할지 애매해서 일단 컴파일 되라고 LocalDateTime.now()
+                .contains(registeredDateTime, 4000);
         assertThat(orderResponse.getProducts()).hasSize(2)
                 .extracting("productNumber", "price")
                 .containsExactlyInAnyOrder(

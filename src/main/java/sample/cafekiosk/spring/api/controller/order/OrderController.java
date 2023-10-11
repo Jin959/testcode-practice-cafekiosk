1cebc4f8316add47dbd76d9d26f0ee3e8f8cbf0d
Jin959(J Lee) <84704897+Jin959@users.noreply.github.com>
2023-10-11 18:02:13 +0900

    주문 생성 테스트 - 패키지 구조 REFACTORING
    - 내가 잘못 넣었던 거 수정

--- src/main/java/sample/cafekiosk/spring/api/controller/order/OrderController.java
+++ src/main/java/sample/cafekiosk/spring/api/controller/order/OrderController.java
@@ -5,7 +5,7 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 import sample.cafekiosk.spring.api.service.order.OrderService;
-import sample.cafekiosk.spring.api.service.order.request.OrderCreateRequest;
+import sample.cafekiosk.spring.api.controller.order.request.OrderCreateRequest;
 
 import java.time.LocalDateTime;
 

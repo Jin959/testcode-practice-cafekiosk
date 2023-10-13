a2a3f1aeacd690c90b4e030313bea02ae0d0005a
Jin959(J Lee) <84704897+Jin959@users.noreply.github.com>
2023-10-13 20:06:22 +0900

    주문 생성 재고 관련 요구사항 추가 - RED
    - TearDown 메서드 잠시 제거하고 @Transactional 사용
    - Stock 도메인 추가

---
+++ src/main/java/sample/cafekiosk/spring/domain/stock/Stock.java
@@ -0,0 +1,39 @@
+package sample.cafekiosk.spring.domain.stock;
+
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import sample.cafekiosk.spring.domain.BaseEntity;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Getter
+@Entity
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class Stock extends BaseEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String productNumber;
+
+    private int quantity;
+
+    @Builder
+    public Stock(String productNumber, int quantity) {
+        this.productNumber = productNumber;
+        this.quantity = quantity;
+    }
+
+    public static Stock create(String number, int quantity) {
+        return Stock.builder()
+                .productNumber(number)
+                .quantity(quantity)
+                .build();
+    }
+}

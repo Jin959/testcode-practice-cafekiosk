38783d4e17e6da438a43478ffd053ff1970a38d3
Jin959(J Lee) <84704897+Jin959@users.noreply.github.com>
2023-10-21 00:11:04 +0900

    주문 생성 재고관련 요구사항 테스트코드 수정 - RED
    - createOrderWithStock 의 금액 및 개수 알맞게 수정
    - 메서드 위치 변경

--- src/test/java/sample/cafekiosk/spring/api/service/order/OrderServiceTest.java
+++ src/test/java/sample/cafekiosk/spring/api/service/order/OrderServiceTest.java
@@ -85,52 +85,6 @@ class OrderServiceTest {
     }
 
     @Test
-    @DisplayName("재고와 관련된 상품이 포함되어 있는 주문번호 리스트를 받아 주문을 생성한다.")
-    void createOrderWithStock() {
-        // given
-        LocalDateTime registeredDateTime = LocalDateTime.now();
-
-        Product product1 = createProduct(BOTTLE, "001", 1000);
-        Product product2 = createProduct(BAKERY, "002", 3000);
-        Product product3 = createProduct(HANDMADE, "003", 5000);
-        productRepository.saveAll(List.of(product1, product2, product3));
-
-        Stock stock1 = Stock.create("001", 2);
-        Stock stock2 = Stock.create("002", 2);
-        stockRepository.saveAll(List.of(stock1, stock2));
-
-        OrderCreateRequest request = OrderCreateRequest.builder()
-                .productNumbers(List.of("001", "001", "002", "003"))
-                .build();
-
-        // when
-        OrderResponse orderResponse = orderService.createOrder(request, registeredDateTime);
-
-        // then
-        assertThat(orderResponse.getId()).isNotNull();
-        assertThat(orderResponse)
-                .extracting("registeredDateTime", "totalPrice")
-                .contains(registeredDateTime, 4000);
-        assertThat(orderResponse.getProducts()).hasSize(2)
-                .extracting("productNumber", "price")
-                .containsExactlyInAnyOrder(
-                        tuple("001", 1000),
-                        tuple("001", 1000),
-                        tuple("002", 3000),
-                        tuple("003", 5000)
-                );
-
-        List<Stock> stocks = stockRepository.findAll();
-        assertThat(stocks).hasSize(2)
-                .extracting("productNumber", "quantity")
-                .containsExactlyInAnyOrder(
-                        tuple("001", 0),
-                        tuple("002", 1)
-                );
-
-    }
-
-    @Test
     @DisplayName("아메리카노 두잔 처럼 중복되는 상품번호 리스트로 주문을 생성할 수 있다.")
     void createOrderWithDuplicateProductNumbers() {
         // given
@@ -159,6 +113,52 @@ class OrderServiceTest {
                         tuple("001", 1000),
                         tuple("001", 1000)
                 );
+    }
+
+    @Test
+    @DisplayName("재고와 관련된 상품이 포함되어 있는 주문번호 리스트를 받아 주문을 생성한다.")
+    void createOrderWithStock() {
+        // given
+        LocalDateTime registeredDateTime = LocalDateTime.now();
+
+        Product product1 = createProduct(BOTTLE, "001", 1000);
+        Product product2 = createProduct(BAKERY, "002", 3000);
+        Product product3 = createProduct(HANDMADE, "003", 5000);
+        productRepository.saveAll(List.of(product1, product2, product3));
+
+        Stock stock1 = Stock.create("001", 2);
+        Stock stock2 = Stock.create("002", 2);
+        stockRepository.saveAll(List.of(stock1, stock2));
+
+        OrderCreateRequest request = OrderCreateRequest.builder()
+                .productNumbers(List.of("001", "001", "002", "003"))
+                .build();
+
+        // when
+        OrderResponse orderResponse = orderService.createOrder(request, registeredDateTime);
+
+        // then
+        assertThat(orderResponse.getId()).isNotNull();
+        assertThat(orderResponse)
+                .extracting("registeredDateTime", "totalPrice")
+                .contains(registeredDateTime, 10000);
+        assertThat(orderResponse.getProducts()).hasSize(4)
+                .extracting("productNumber", "price")
+                .containsExactlyInAnyOrder(
+                        tuple("001", 1000),
+                        tuple("001", 1000),
+                        tuple("002", 3000),
+                        tuple("003", 5000)
+                );
+
+        List<Stock> stocks = stockRepository.findAll();
+        assertThat(stocks).hasSize(2)
+                .extracting("productNumber", "quantity")
+                .containsExactlyInAnyOrder(
+                        tuple("001", 0),
+                        tuple("002", 1)
+                );
+
     }
 
     private Product createProduct(ProductType type, String productNumber, int price) {

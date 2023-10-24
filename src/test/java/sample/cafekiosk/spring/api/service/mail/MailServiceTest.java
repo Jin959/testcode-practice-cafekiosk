e04693a7e9bf89e1fb108cb9457142250e587b52
Jin959(J Lee) <84704897+Jin959@users.noreply.github.com>
2023-10-24 22:16:57 +0900

    일매출 메일링 기능 추가 - 메일 서비스 객체 @Mock, @InjectMocks

--- src/test/java/sample/cafekiosk/spring/api/service/mail/MailServiceTest.java
+++ src/test/java/sample/cafekiosk/spring/api/service/mail/MailServiceTest.java
@@ -2,7 +2,10 @@ package sample.cafekiosk.spring.api.service.mail;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
-import org.mockito.Mockito;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
 import sample.cafekiosk.spring.client.mail.MailSendClient;
 import sample.cafekiosk.spring.domain.history.mail.MailSendHistory;
 import sample.cafekiosk.spring.domain.history.mail.MailSendHistoryRepository;
@@ -12,20 +15,24 @@ import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.ArgumentMatchers.anyString;
 import static org.mockito.Mockito.*;
 
+@ExtendWith(MockitoExtension.class)
 class MailServiceTest {
+
+    @Mock
+    private MailSendClient mailSendClient;
+
+    @Mock
+    private MailSendHistoryRepository mailSendHistoryRepository;
+
+    @InjectMocks
+    private MailService mailService;
 
     @Test
     @DisplayName("메일 전송 테스트")
     void sendMail() {
         // given
-        MailSendClient mailSendClient = Mockito.mock(MailSendClient.class);
-        MailSendHistoryRepository mailSendHistoryRepository = Mockito.mock(MailSendHistoryRepository.class);
-
-        MailService mailService = new MailService(mailSendClient, mailSendHistoryRepository);
-
         when(mailSendClient.sendEmail(anyString(), anyString(), anyString(), anyString()))
                 .thenReturn(true);
-
 
         // when
         Boolean result = mailService.sendMail("", "", "", "");

cf714b4e2c7b9ffe7a4888613d7bcbc3a0e8bdae
Jin959(J Lee) <84704897+Jin959@users.noreply.github.com>
2023-10-24 19:22:50 +0900

    일매출 메일링 기능 추가 - 메일 서비스 객체 순수 Mockito로 검증
    - Mail 을 전송하는 클라이언트를 호출하고 전송을 저장하는 메일 서비스
      테스트
      - 스프링 컨테이너와 빈 없이 순수 Mockito 검

---
+++ src/test/java/sample/cafekiosk/spring/api/service/mail/MailServiceTest.java
@@ -0,0 +1,38 @@
+package sample.cafekiosk.spring.api.service.mail;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
+import sample.cafekiosk.spring.client.mail.MailSendClient;
+import sample.cafekiosk.spring.domain.history.mail.MailSendHistory;
+import sample.cafekiosk.spring.domain.history.mail.MailSendHistoryRepository;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.ArgumentMatchers.anyString;
+import static org.mockito.Mockito.*;
+
+class MailServiceTest {
+
+    @Test
+    @DisplayName("메일 전송 테스트")
+    void sendMail() {
+        // given
+        MailSendClient mailSendClient = Mockito.mock(MailSendClient.class);
+        MailSendHistoryRepository mailSendHistoryRepository = Mockito.mock(MailSendHistoryRepository.class);
+
+        MailService mailService = new MailService(mailSendClient, mailSendHistoryRepository);
+
+        when(mailSendClient.sendEmail(anyString(), anyString(), anyString(), anyString()))
+                .thenReturn(true);
+
+
+        // when
+        Boolean result = mailService.sendMail("", "", "", "");
+
+        // then
+        assertThat(result).isTrue();
+        verify(mailSendHistoryRepository, times(1)).save(any(MailSendHistory.class));
+    }
+
+}
